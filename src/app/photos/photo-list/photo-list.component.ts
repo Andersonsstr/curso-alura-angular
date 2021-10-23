596b81c52dc31d9028bfd4c304d8bb8ab84acb7a
Anderson Silva <andersonsstr@gmail.com>
2021-10-23 16:54:47 -0300

    Load button funcionando

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -12,10 +13,14 @@ export class PhotoListComponent implements OnInit {
 
   photos:Photo[] = [];
   filter: string = '';
+  hasMore:boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private photosService: PhotoService) { }
   
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 
@@ -24,5 +29,12 @@ export class PhotoListComponent implements OnInit {
       var elemento = target as HTMLInputElement;
       this.filter = elemento.value;
     }
+  }
+
+  load(){
+    this.photosService.listFromPaginated(this.userName, ++this.currentPage).subscribe(photos => {
+      this.photos = this.photos.concat(photos);
+      if(!photos.length) this.hasMore = false;
+    });
   }
 }
